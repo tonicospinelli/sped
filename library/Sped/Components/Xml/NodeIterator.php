9b49c43b8c7e3759ff82d644ebe96b2782057029
Antonio Spinelli <tonicospinelli85@gmaill.com>
2012-06-06 09:23:25 -0300

    Commons\Xml refatorada para Components\Xml

---
+++ library/Sped/Components/Xml/NodeIterator.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Sped
+ *
+ * Copyright (c) 2012 Sped
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category   Sped
+ * @package    Sped
+ * @copyright  Copyright (c) 2012 Sped (https://github.com/tonicospinelli/Sped)
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
+ * @version    ##VERSION##, ##DATE##
+ */
+
+namespace Sped\Components\Xml;
+
+/**
+ * @category   Sped
+ * @package    Sped
+ * @copyright  Copyright (c) 2012
+ * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
+ * @author     Antonio Spinelli <tonicospinelli85@gmail.com>
+ */
+class NodeIterator implements RecursiveIterator {
+
+    /**
+     * Current Position in DOMNodeList
+     * @var Integer
+     */
+    protected $_position;
+
+    /**
+     * The DOMNodeList with all children to iterate over
+     * @var DOMNodeList
+     */
+    protected $_nodeList;
+
+    /**
+     * @param DOMNode $domNode
+     * @return void
+     */
+    public function __construct(\DOMNode $domNode) {
+        $this->_position = 0;
+        $this->_nodeList = $domNode->childNodes;
+    }
+
+    /**
+     * Returns the current DOMNode
+     * @return DOMNode
+     */
+    public function current() {
+        return $this->_nodeList->item($this->_position);
+    }
+
+    /**
+     * Returns an iterator for the current iterator entry
+     * @return RecursiveDOMIterator
+     */
+    public function getChildren() {
+        return new self($this->current());
+    }
+
+    /**
+     * Returns if an iterator can be created for the current entry.
+     * @return Boolean
+     */
+    public function hasChildren() {
+        return $this->current()->hasChildNodes();
+    }
+
+    /**
+     * Returns the current position
+     * @return Integer
+     */
+    public function key() {
+        return $this->_position;
+    }
+
+    /**
+     * Moves the current position to the next element.
+     * @return void
+     */
+    public function next() {
+        $this->_position++;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @return void
+     */
+    public function rewind() {
+        $this->_position = 0;
+    }
+
+    /**
+     * Checks if current position is valid
+     * @return Boolean
+     */
+    public function valid() {
+        return $this->_position < $this->_nodeList->length;
+    }
+
+}
