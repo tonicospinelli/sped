89a60d9e0c0a116c00c06630f53124d1deac6e4f
Antonio Oertel Spinelli <tonicospinelli85@gmail.com>
2012-06-06 11:26:46 -0300

    \DOMElement refatorado para \Sped\Components\Xml\Element

--- library/Sped/Schemas/V200/Signature/KeyInfo/X509Certificate.php
+++ library/Sped/Schemas/V200/Signature/KeyInfo/X509Certificate.php
@@ -36,7 +36,7 @@ namespace Sped\Schemas\V200\Signature\KeyInfo;
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
  * @author     Antonio Spinelli <tonicospinelli85@gmail.com>
  */
-class X509Certificate extends \DOMElement {
+class X509Certificate extends \Sped\Components\Xml\Element {
 
     public function __construct() {
         parent::__construct('X509Certificate', null, 'http://www.w3.org/2000/09/xmldsig#');
@@ -55,8 +55,8 @@ class X509Certificate extends \DOMElement {
      * @param string $key
      * @return \Sped\Schemas\V200\Signature\KeyInfo\X509Certificate 
      */
-    public function setValue($key) {
-        $this->nodeValue = $key;
+    public function setValue($paramString) {
+        $this->nodeValue = $paramString;
         return $this;
     }
 
