f1d34ff50644b12bd1c7d04bc24577f702a01b1e
Antonio Spinelli <tonicospinelli85@gmaill.com>
2012-06-11 08:40:50 -0300

    Novo método para remover os Nós filhos com base na TagName

--- library/Sped/Components/Xml/Document.php
+++ library/Sped/Components/Xml/Document.php
@@ -118,6 +118,22 @@ class Document extends \DOMDocument {
     }
 
     /**
+     * (PHP 5)<br/>
+     * Removes children by tag name from list of children
+     * @link http://php.net/manual/en/domnode.removechild.php
+     * @param string $tagName <p>
+     * The tagName to remove children.
+     * </p>
+     * @return boolean If the child could be removed the function returns true.
+     */
+    public function removeChildrenByTagName($name) {
+        $nodes = $this->getElementsByTagName($name);
+        foreach ($nodes as $node)
+            $this->removeChild($node);
+        return true;
+    }
+
+    /**
      * Return array of namespaces of the document
      * @return array
      */
