b4df4a18632c046f2049d5596d76a65cf870d4ba
Antonio Oertel Spinelli <tonicospinelli85@gmail.com>
2012-08-08 11:26:26 -0300

    Teste com a mascara e geração de código númerico

--- tests/library/Sped/Commons/Documents/ChaveAcessoTest.php
+++ tests/library/Sped/Commons/Documents/ChaveAcessoTest.php
@@ -32,14 +32,23 @@ class ChaveAcessoTest extends \PHPUnit_Framework_TestCase
         
     }
 
-    public function testGetValueMasked()
+    public function testGetValueUnmasked()
     {
-        $this->assertEquals('NFe11101284613439000180550010000004881093997017', $this->object->getValueMasked());
+        var_dump($this->object->getValueMasked());
+        $this->assertEquals('11101284613439000180550010000004881093997017', $this->object->getValueUnmasked());
     }
 
     public function testIsValid()
     {
         $this->assertTrue($this->object->isValid());
+    }
+
+    public function testGerarCodigoNumerico()
+    {
+        $nfe = new \Sped\Schemas\V200\DocumentNFe();
+        $nfe->load('samples/35101158716523000119550010000000011003000000-nfe.xml');
+        
+        var_dump($this->object->gerarCodigoNumerico($nfe));
     }
 
 }
